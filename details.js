3354ef74448f5226dda5a204a5dc32c03ce6b281
pierluigi991 <marzo.pierluigi991@gmail.com>
2023-03-20 09:11:59 +0100

    second

--- details.js
+++ details.js
@@ -1,1 +1,56 @@
 //da completare
+const id = new URLSearchParams(window.location.search).get('_id')
+const apiURL = 'https://striveschool-api.herokuapp.com/api/product/'
+const a = {
+    headers: {
+        "Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MjZjZWY4MWI0MjAwMTM5YjI3ZDkiLCJpYXQiOjE2NzkwNjAzMDksImV4cCI6MTY4MDI2OTkwOX0.d4QTbGcZHATHanH_5rVhS3osWFUCxFsyczVVUE7seV0",
+        }
+}
+
+fetch(apiURL + id, a)
+.then(response => response.json())
+.then(pic => {
+    const container = document.getElementById('container')
+
+    const picture = document.createElement('figure')
+    picture.className = 'zoom'
+    picture.style.backgroundImage = `url('${pic.imageUrl}')`
+
+    const img = document.createElement('img')
+    img.src = pic.imageUrl
+
+    picture.appendChild(img)
+
+    
+    picture.addEventListener('mousemove',(e)=>{
+        let zoomer = e.currentTarget;
+        e.offsetX ? offsetX = e.offsetX : offsetX = e.touches
+        e.offsetY ? offsetY = e.offsetY : offsetX = e.touches
+        x = offsetX/zoomer.offsetWidth*100
+        y = offsetY/zoomer.offsetHeight*100
+        zoomer.style.backgroundPosition = x + '%' + y + '%';
+    })
+
+    const images = [img,picture]
+    images.forEach((element)=>{
+        element.style.width = '300px'
+        element.style.height = '300px'
+    })
+
+    const h2 = document.createElement('h2')
+    h2.innerText = pic.name
+
+    const b = document.createElement('p')
+    b.innerText = pic.brand
+
+    const p = document.createElement('p')
+    p.innerText = pic.description
+
+    const all = [figure,h2,b,p]
+
+    all.forEach((element)=>{
+        element.className = 'm-auto mt-5'
+    })
+
+    container.append(figure,h2,b,p)
+})
--- index2.js
+++ index2.js
@@ -1,49 +1,3 @@
-// const apiURL = "https://striveschool-api.herokuapp.com/api/product/";
-// const a = {
-//   headers: {
-//     Authorization:
-//       "Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MjZjZWY4MWI0MjAwMTM5YjI3ZDkiLCJpYXQiOjE2NzkwNjAzMDksImV4cCI6MTY4MDI2OTkwOX0.d4QTbGcZHATHanH_5rVhS3osWFUCxFsyczVVUE7seV0",
-//   },
-// };
-
-// fetch(apiURL, a)
-//   .then((response) => response.json())
-//   .then((products) => {
-//     const grid = document.getElementById("list");
-//     grid.firstElementChild.remove();
-
-//     products.forEach((product) => {
-//       const grid = document.getElementById("list");
-//       const main = document.createElement("div");
-//       const cardBody = document.createElement("div");
-//       const img = document.createElement("img");
-//       const h4 = document.createElement("h5");
-//       const p = document.createElement("p");
-//       const divDispalyFlex = document.createElement("div");
-//       const price = document.createElement("p");
-//       const btnEdit = document.createElement("button");
-
-//       main.style.width = "18rem";
-//       h4.style.textAlign = "center";
-
-//       main.classList.add("card", "m-3");
-//       divDispalyFlex.classList.add("d-flex", "justify-content-between", "align-items-baseline");
-//       btnEdit.classList.add("btn", "btn-primary");
-//       cardBody.className = "card-body";
-//       img.className = "card-img-top";
-
-//       img.src = product.imageUrl;
-//       h4.innerText = product.name;
-//       p.innerText = product.description;
-//       price.innerText = product.price + "$";
-//       btnEdit.innerText = "Edit";
-
-//       grid.append(main);
-//       main.append(img, cardBody);
-//       cardBody.append(h4, p, divDispalyFlex);
-//       divDispalyFlex.append(price, btnEdit);
-//     });
-//   });
 const apiURL = "https://striveschool-api.herokuapp.com/api/product/";
 const a = {
   headers: {
@@ -69,9 +23,6 @@ fetch(apiURL, a)
       const price = document.createElement("p");
       const btnEdit = document.createElement("button");
       const details = document.createElement("a");
-
-      divMain.style.width = "18rem";
-      h5.style.textAlign = "center";
 
       divMain.classList.add("card", "m-3");
       divFlex.classList.add("d-flex", "justify-content-between", "align-items-baseline");
